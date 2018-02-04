58234e477cf35c01698836a2de18e8cc97bfcbc1
ST-tom <2680754348@qq.com>
2018-02-04 17:23:49 +0800

    excel helper

--- TS/TS.Web/Controllers/CommonController.cs
+++ TS/TS.Web/Controllers/CommonController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using NPOI.XSSF.UserModel;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using TS.Service.Files;
 
 namespace TS.Web.Controllers
 {
@@ -22,5 +26,60 @@ namespace TS.Web.Controllers
 
             return View();
         }
+
+        #region Demo Excel
+
+        public void Export()
+        {
+            XSSFWorkbook xk = new ExcelHelper().Export("sheet1",new string[] {"test1","test2","test3" },new string[] { "Name","Sex","Age"},new List<dynamic>().AsQueryable());
+
+            //写入到客户端
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                var fileName = "测试excel"; 
+                if (HttpContext.Request.Browser.Browser == "IE")
+                    fileName = HttpUtility.UrlEncode(fileName);
+
+                xk.Write(ms);
+                Response.AddHeader("Content-Disposition", string.Format("attachment; filename={0}.xls", fileName));
+                Response.BinaryWrite(ms.ToArray());
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Import()
+        {
+            var postFile = Request.Files[0];
+            StringBuilder errmsg = new StringBuilder();
+            if (postFile != null)
+            {
+                string exName = postFile.FileName.Split('.')[1];
+                if (exName == ".xlsx")
+                {
+                    if (postFile.ContentLength / 1024 <= 1024 * 5) //5MB
+                    {
+                        //如果文件较大应该先保存在本地，再进行读写操作
+                        XSSFWorkbook xb = new XSSFWorkbook(postFile.InputStream);
+                        var list = new ExcelHelper().Import<dynamic>(xb, new Dictionary<string, string>(), out errmsg);
+                    }
+                    else
+                    {
+                        errmsg.Append("文件过大");
+                    }
+                }
+                else
+                {
+                    errmsg.Append("文件格式有误");
+                }
+            }
+            else
+            {
+                errmsg.Append("未上传文件");
+            }
+
+            return Json(new { result = errmsg.Length == 0, errmsg = errmsg });
+        }
+
+        #endregion
     }
 }
