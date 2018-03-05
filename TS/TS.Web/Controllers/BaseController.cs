474736dbad4c14cf71a83f8753398cf611bd5da4
ST-tom <2680754348@qq.com>
2018-03-05 15:17:13 +0800

    添加分页渲染

--- TS/TS.Web/Controllers/BaseController.cs
+++ TS/TS.Web/Controllers/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,27 @@ namespace TS.Web.Controllers
     [HttpExceptionFilter]
     public class BaseController : Controller
     {
+        protected JsonResult NoAuthorityJson(string errmsg = "无操作权限")
+        {
+            return Json(new { result = false, errmsg = errmsg }, JsonRequestBehavior.AllowGet);
+        }
 
+        protected virtual string RenderPartialViewToString(string viewName, object model)
+        {
+            //Original source code: http://craftycodeblog.com/2010/05/15/asp-net-mvc-render-partial-view-to-string/
+            if (string.IsNullOrEmpty(viewName))
+                viewName = this.ControllerContext.RouteData.GetRequiredString("action");
+
+            this.ViewData.Model = model;
+
+            using (var sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = System.Web.Mvc.ViewEngines.Engines.FindPartialView(this.ControllerContext, viewName);
+                var viewContext = new ViewContext(this.ControllerContext, viewResult.View, this.ViewData, this.TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
     }
 }
